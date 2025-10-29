e00bf3e63feb20dde1e3b525b8e5c2ce58f6cd8b
يوسف ياسر <yassefsea274@gmail.com>
2025-10-29 17:31:18 +0300

    Update start.js

--- start.js
+++ start.js
@@ -30,7 +30,5 @@ app.use((err, req, res, next) => {
   console.error("Server Error:", err.message);
   res.status(500).json({ message: "Internal Server Error" });
 });
-
-app.listen(3001, () => {
-  console.log('Server is running on port 3001');
-});
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
