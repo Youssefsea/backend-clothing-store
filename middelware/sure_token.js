246cc388a55d2110fc2e285a6fdd0cf758eb7431
Youssef Yasser <youremail@example.com>
2025-11-23 19:01:40 +0200

    change at joi add to cart

--- middelware/sure_token.js
+++ middelware/sure_token.js
@@ -24,8 +24,7 @@ const validate = (schema) => (req, res, next) => {
   const { error } = schema.validate(req.body, { abortEarly: false });
   if (error) {
     return res.status(400).send({
-      body: req.body,
-      message: 'Validation error',
+      message: 'Validation error error',
       details: error.details.map((d) => d.message),
     });
   }
