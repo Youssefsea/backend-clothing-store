cd8da30c4b32c503b1fa6f460fa56d9b3a692dae
Youssef Yasser <youremail@example.com>
2025-11-26 14:34:37 +0200

    add user info

--- Data/data.js
+++ Data/data.js
@@ -1,7 +1,7 @@
 const { Pool } = require('pg');
 require('dotenv').config();
 
-const pool = new Pool({
+const data = new Pool({
   host: process.env.PGHOST,
   user: process.env.PGUSER,
   password: process.env.PGPASSWORD,
@@ -12,7 +12,7 @@ const pool = new Pool({
   },
 });
 
-pool.connect()
+data.connect()
   .then(client => {
     console.log('Connected successfully to PostgreSQL');
     client.release();
@@ -21,23 +21,6 @@ pool.connect()
     console.error('Database failed:', err.message);
   });
 
-function convertPlaceholders(query, values) {
-  let idx = 0;
-  return query.replace(/\?/g, () => {
-    idx++;
-    return `$${idx}`;
-  });
-}
 
-async function query(sql, params = []) {
-  const text = convertPlaceholders(sql, params);
-  const client = await pool.connect();
-  try {
-    const result = await client.query(text, params);
-    return result; // Return the full result object with .rows property
-  } finally {
-    client.release();
-  }
-}
 
-module.exports = { query };
+module.exports = data;
